5edfc5e4852b739be3dbd968706284638bb5eb64
yoojin <ffzon@webisfree.com>
2020-12-21 14:22:02 +0900

    비밀번호 보호

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express') //express 가져옴
 const app = express() //앱을만듬
 const port = 4000 //포트설정해줌
+const bodyParser = require('body-parser');
+const config = require("./config/key");
+
+//application/x-www-form-urlencoded 라는 데이터를 분석해서 가져올수있게 해주는것
+app.use(bodyParser.urlencoded({ extended: true,}));
+
+//application/json 이렇게 된것을 분석해서 가져올수 있게 해주는것
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose'); 
-mongoose.connect('mongodb+srv://sooin:mimiru0502@boiler-plate.clt2w.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true,
@@ -15,8 +23,34 @@ mongoose.connect('mongodb+srv://sooin:mimiru0502@boiler-plate.clt2w.mongodb.net/
 });
 
 app.get('/', (req, res) => { //req(요청) 및 res(응답)
-  res.send('Hello World~~~!')
+  res.send('헬로월드')
 })
+
+app.post("/register", (req, res) => {
+   //회원가입 할때 필요한 정보를 client에서 가져오면
+   //그것들을 데이터베이스에 넣어줍니다.
+   /*
+   req.body -->
+    {id:"hello"
+    pasword:"123"} 이런형태
+   */
+   const user = new User(req.body);
+   console.log(user);
+   user.save((err,userInfo) => {
+      if(err) return res.json({success: false, err});
+      
+      //sucess를 false로 알려주고 err내용을 알려준다.
+      //에러가 없으면
+      
+      //status(200) 성공했따.
+      return res.status(200).json({
+          success:true
+      });
+
+    });
+
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
